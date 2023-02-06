3ebb88f6785a081954de98c7a2d2517b52aee440
2pique <2piquz@my.com>
2023-02-06 03:36:57 +0100

    updated the models scheam

--- jobs/models.py
+++ jobs/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Provider(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
     def __str__(self):
         return self.name
 
 
 class Job(models.Model):
-    title = models.CharField(max_length=50)
-    company = models.CharField(max_length=50)
-    location = models.CharField(max_length=50)
-    link = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True,)
     def __str__(self):
